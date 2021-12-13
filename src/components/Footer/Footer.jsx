217072277a30571e92c9a15657c73fea1897ad44
sungmin-choi <namja306@naver.com>
2021-12-13 23:52:04 +0900

    Feat)로그인 페이지 구현 완료

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import styles from './Footer.module.css';
 const Footer = (props) => {
     return(
-        <footer>
-            <p>Code your dream</p>
+        <footer className={styles.footer}>
+            <p className={styles.footerText}>Code your dream</p>
         </footer>
     )
 };
