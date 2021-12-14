57a05f02e6f4e3508acf2d57c0d18f57e9c84840
sungmin-choi <namja306@naver.com>
2021-12-14 09:28:13 +0900

    Feat) 로그아웃 기능 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import styles from './Header.module.css';
 import { faIdCard } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {authUser} from "../../fbase";
+import {signOut } from "firebase/auth";
 const Header = ({isLoggin}) => {
+
+    const logOut = async()=>await signOut(authUser);
+
     return(
         <header className={styles.header}>
         <FontAwesomeIcon  icon={faIdCard} size='3x'/>
         <p className={styles.description}>Business Card Maker</p>
-        {isLoggin && <button>log out</button>}
+        {isLoggin && <button onClick={logOut} className={styles.logoutBtn}>log out</button>}
         </header>
     )
 };
