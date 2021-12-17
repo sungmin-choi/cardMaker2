d9661858cb9b92251df82b84c44dadb77120860c
sungmin-choi <namja306@naver.com>
2021-12-17 13:22:28 +0900

    Refact) app.js 리팩토링

--- src/App.js
+++ src/App.js
@@ -2,28 +2,19 @@ import React, { useEffect, useState } from 'react';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import EditPage from './routes/EditPage/EditPage';
-import {authUser} from './fbase';
-import {onAuthStateChanged } from "firebase/auth";
+import FirebaseService from './fbase';
 import LoginPage from './routes/LoginPage/LoginPage';
 import styles from './App.module.css';
+
+const firebase = new FirebaseService();
+
 function App() {
   const [init,setInit]=useState(false);
   const [isLoggin,setIsLoggin]=useState(false);
   const [userObj,setUserObj] = useState(null);
 
   useEffect(()=>{
-    onAuthStateChanged(authUser, (user) => {
-      if (user) {
-        const userObj={
-          userId:user.uid,
-          createAt:user.metadata.createdAt,
-        }
-        setUserObj(userObj);
-        setIsLoggin(true);
-      } else {
-        setIsLoggin(false);
-      }
-    });
+    firebase.stateChanged(setUserObj,setIsLoggin);
     setInit(true);
   },[]);
 
--- src/fbase.js
+++ src/fbase.js
@@ -2,6 +2,42 @@
 import { initializeApp } from "firebase/app";
 import { getAuth} from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import {onAuthStateChanged } from "firebase/auth";
+
+export default class FirebaseService{
+  constructor(){
+    this.firebaseConfig = {
+      apiKey: "AIzaSyDw7tricSnJQqCvC2-mgjJ8tpGUhCTOSU4",
+      authDomain: "cardmaker2-c1863.firebaseapp.com",
+      databaseURL: "https://cardmaker2-c1863-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "cardmaker2-c1863",
+      storageBucket: "cardmaker2-c1863.appspot.com",
+      messagingSenderId: "340955878649",
+      appId: "1:340955878649:web:2f2cf7109cde333d833e28"
+    };
+    this.app=initializeApp(firebaseConfig);
+    this.authUser = getAuth();
+    this.cardDb = getDatabase(app); 
+  }
+
+  stateChanged(setUserObj,setIsLoggin){
+    onAuthStateChanged(this.authUser, (user) => {
+      if (user) {
+        const userObj={
+          userId:user.uid,
+          createAt:user.metadata.createdAt,
+        }
+        setUserObj(userObj);
+        setIsLoggin(true);
+      } else {
+        setIsLoggin(false);
+      }
+    });
+  }
+
+
+}
+
 const firebaseConfig = {
   apiKey: "AIzaSyDw7tricSnJQqCvC2-mgjJ8tpGUhCTOSU4",
   authDomain: "cardmaker2-c1863.firebaseapp.com",
